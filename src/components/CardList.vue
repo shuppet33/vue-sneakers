99ebd5c6955f4a3b6644c3c924355b5e040ae50d
Alena <nikishova-alena@mail.ru>
2025-02-06 00:10:33 +0300

    первый коммит

---
+++ src/components/CardList.vue
@@ -0,0 +1,17 @@
+<script setup>
+   import Card from './Card.vue'
+   const onClickAdd = () => {
+      alert(1)
+   }
+</script>
+
+<template>
+      <div class="grid grid-cols-4 gap-5">
+         <Card  
+            :imageUrl="'/sneakers/sneakers-1.jpg'" title="Мужские Кроссовки Nike Blazer Mid Suede" :price="5000"
+            :isAdded="false"
+            :isFavorite="false"
+            :onClickAdd="onClickAdd"
+         />
+      </div>
+</template>
